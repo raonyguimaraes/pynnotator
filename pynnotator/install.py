7a400d2a6315db8dce6015ab83dbcf206420f0ee
Raony Guimaraes <raony.guimaraes@genomicsmed.ie>
2017-04-11 15:20:10 +0100

    fix vep

--- pynnotator/install.py
+++ pynnotator/install.py
@@ -205,7 +205,7 @@ class Installer(object):
 
             os.chdir(settings.vep_dir)
             # download vep cache
-            command = """perl INSTALL.pl -a a --NO_HTSLIB"""
+            command = """perl INSTALL.pl -a a"""
             call(command, shell=True)
 
 
