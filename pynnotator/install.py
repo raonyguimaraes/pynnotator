040ee52f3b522ed264c327275b9aaa9c93464fa9
Raony Guimaraes <raonyguimaraes@gmail.com>
2017-04-15 01:51:32 +0000

    updates

--- pynnotator/install.py
+++ pynnotator/install.py
@@ -33,8 +33,8 @@ class Installer(object):
 
     def install_requirements(self):
         """Install Ubuntu Requirements"""
-        print('Installing Ubuntu requirements')
-        if platform.dist()[0] in ['Debian', 'Ubuntu', 'LinuxMint']:
+        print('Installing Requirements')
+        if platform.dist()[0] in ['Ubuntu', 'LinuxMint']:
             command = 'sudo apt-get install sed python3-dev python3-pip python3-setuptools vcftools bcftools tabix zlib1g-dev libpq-dev build-essential zlib1g-dev liblocal-lib-perl cpanminus curl unzip wget'  # lamp-server^
             sts = call(command, shell=True)
 
