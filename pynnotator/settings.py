3c9111e249d4ef297c819b4796d1a66664cae4c2
Raony Guimaraes <raony.guimaraes@genomicsmed.ie>
2017-12-13 16:32:04 +0000

    update VEP to version 91

--- pynnotator/settings.py
+++ pynnotator/settings.py
@@ -50,7 +50,7 @@ snpeff_dir = os.path.join(libs_dir, 'snpeff', 'snpEff')
 snpeff_data_dir = os.path.join(data_dir, 'snpeff_data')
 
 # vep
-vep_release = '90.10'
+vep_release = '91'
 vep_source = 'https://github.com/Ensembl/ensembl-vep/archive/release/%s.zip' % (vep_release)
 vep_cache_dir = os.path.join(data_dir, 'vep_cache')
 vep_dir = '%s/vep/ensembl-vep-release-%s/' % (libs_dir, vep_release)
