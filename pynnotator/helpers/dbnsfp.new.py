8d395c9e7ddd34f9f748b6d83b30ed667611cb01
Raony Guimaraes <raonyguimaraes@gmail.com>
2018-05-24 23:43:01 +0100

    before merge

--- pynnotator/helpers/dbnsfp.new.py
+++ pynnotator/helpers/dbnsfp.new.py
@@ -13,17 +13,15 @@ from subprocess import run
 
 class Dbnsfp(object):
     def __init__(self, vcf_file=None, cores=None):
-    
-        self.vcf_file = vcf_file
-        self.dbnsfp_header = open('%s/dbnsfp/header.vcf' % (settings.data_dir)).readlines()
 
+        self.vcf_file = vcf_file
         # print('self.resources', self.resources)
         self.cores = int(cores)
 
         self.filename = os.path.splitext(os.path.basename(str(vcf_file)))[0]
         # create folder validator if it doesn't exists
-        if not os.path.exists('dbnsfp'):
-            os.makedirs('dbnsfp')
+        if not os.path.exists('dbnfsp'):
+            os.makedirs('dbnfsp')
 
     def run(self):
 
@@ -33,26 +31,26 @@ class Dbnsfp(object):
 
         # std = self.annotator()
 
-        self.splitvcf(self.vcf_file)
+        # self.splitvcf(self.vcf_file)
 
-        pool = mp.Pool()
-        pool.map(self.annotate, range(1, self.cores + 1))
-        # pool.close()
-        # pool.join()
+        # pool = mp.Pool()
+        # pool.map(self.annotate, range(1, self.cores + 1))
+        # # pool.close()
+        # # pool.join()
 
-        prefix = 'dbnsfp'
-        # # Define your jobs
-        # jobs = []
-        final_parts = []
-        for n in range(0, self.cores):
-            index = n + 1
-            final_file = 'dbnsfp/dbnsfp.%s.vcf' % (index)
-            final_parts.append(final_file)
+        # prefix = 'dbnfsp'
+        # # # Define your jobs
+        # # jobs = []
+        # final_parts = []
+        # for n in range(0, self.cores):
+        #     index = n + 1
+        #     final_file = 'dbnfsp/dbnfsp.%s.vcf' % (index)
+        #     final_parts.append(final_file)
 
-        command = 'cat %s/header.vcf ' % (prefix) + " ".join(final_parts) + '> %s/dbnsfp.vcf' % (prefix)
-        run(command, shell=True)
+        command = '%s/vcfanno_linux64 ' % (settings.vcfanno_dir)
+        std = os.system(command)
 
-        command = 'rm %s/header.vcf %s/body.vcf %s/dbnsfp.*.vcf %s/part.*' % (prefix, prefix, prefix, prefix)
+        # command = 'rm %s/header.vcf %s/body.vcf %s/dbnfsp.*.vcf %s/part.*' % (prefix, prefix, prefix, prefix)
         # run(command, shell=True)
 
         tend = datetime.now()
@@ -66,7 +64,7 @@ class Dbnsfp(object):
     def splitvcf(self, vcffile):
         # print('split file', vcffile)
         # print 'numero de cores', cores
-        prefix = 'dbnsfp'
+        prefix = 'dbnfsp'
         vcf_reader = open('%s' % (vcffile))
         header_writer = open('%s/header.vcf' % (prefix), 'w')
         body_writer = open('%s/body.vcf' % (prefix), 'w')
@@ -75,7 +73,7 @@ class Dbnsfp(object):
         for line in vcf_reader:
             if line.startswith('#'):
                 if line.startswith('#CHROM'):
-                    header_writer.writelines(self.dbnsfp_header)
+                    header_writer.writelines(self.dbnfsp_header)
                 header_writer.writelines(line)
             else:
                 body_writer.writelines(line)
@@ -97,7 +95,7 @@ class Dbnsfp(object):
     # convert and annotate the vcf file to snpeff
     def annotate(self, out_prefix):
         # print 'Hello'
-        # print self.dbnsfp_reader
+        # print self.dbnfsp_reader
         # header is at:
 
         # 24    SIFT_score: SIFT score (SIFTori).
@@ -107,23 +105,23 @@ class Dbnsfp(object):
         # 191 clinvar_golden_stars: ClinVar Review Status summary.
 
         # print 'input',vcffile, out_prefix, dbnsfp 
-        dbnsfp_reader = pysam.Tabixfile(settings.dbnsfp, 'r', encoding='utf-8')
+        dbnfsp_reader = pysam.Tabixfile(settings.dbnsfp, 'r', encoding='utf-8')
 
         # print('header')
-        for item in dbnsfp_reader.header:
+        for item in dbnfsp_reader.header:
             header = item.strip().split('\t')
 
-        # header = dbnsfp_reader.header.next().strip().split('\t')
+        # header = dbnfsp_reader.header.next().strip().split('\t')
 
-        vcffile = 'dbnsfp/part.%s.vcf' % (out_prefix)
+        vcffile = 'dbnfsp/part.%s.vcf' % (out_prefix)
 
         vcf_reader = open('%s' % (vcffile))
-        vcf_writer = open('dbnsfp/dbnsfp.%s.vcf' % (out_prefix), 'w', encoding="utf-8")
+        vcf_writer = open('dbnfsp/dbnfsp.%s.vcf' % (out_prefix), 'w', encoding="utf-8")
 
         for line in vcf_reader:
             if line.startswith('#'):
                 if line.startswith('#CHROM'):
-                    vcf_writer.writelines(dbnsfp_header)
+                    vcf_writer.writelines(dbnfsp_header)
                 vcf_writer.writelines(line)
             else:
                 variant = line.split('\t')
@@ -131,7 +129,7 @@ class Dbnsfp(object):
                 index = '%s-%s' % (variant[0], variant[1])
                 # print index
                 try:
-                    records = dbnsfp_reader.fetch(variant[0], int(variant[1]) - 1, int(variant[1]),multiple_iterators=True)
+                    records = dbnfsp_reader.fetch(variant[0], int(variant[1]) - 1, int(variant[1]))
                 except:
                     records = []
 
@@ -155,7 +153,6 @@ class Dbnsfp(object):
                                 new_ann.append('dbNSFP_%s=%s' % (item, ann[idx].replace(';', '|')))
                         variant[7] = '%s;%s' % (variant[7], ";".join(new_ann))
                 vcf_writer.writelines("\t".join(variant))
-        vcf_writer.close()
 
 
 if __name__ == '__main__':
