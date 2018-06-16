937658a491f53d3ac9630fdef8b593b35a05aa11
Raony Guimaraes <raonyguimaraes@gmail.com>
2018-06-16 08:54:42 +0000

    fix installation script

--- Dockerfile
+++ Dockerfile
@@ -32,6 +32,6 @@ RUN apt-get update && \
 
 COPY . /pynnotator
 WORKDIR /pynnotator
-RUN python3 setup.py develop
+RUN python3 setup.py install
 RUN pynnotator install
 ENTRYPOINT ["pynnotator"]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='pynnotator',
-      version='1.4',
+      version='1.5',
       description='A Python Annotation Framework for VCFs using multiple tools (Ex. VEP, SnpEff and SnpSift) and databases (Ex. 1000genomes, dbSNP and dbnfsp) .',
       url='http://github.com/raonyguimaraes/pynnotator',
       author='Raony Guimaraes',
