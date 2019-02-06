3823167a75330eb80aab2eac908b8fd85c23a589
Raony Guimaraes <raonyguimaraes@gmail.com>
2019-02-06 00:10:44 +0000

    Update install.sh

--- pynnotator/scripts/install.sh
+++ pynnotator/scripts/install.sh
@@ -2,7 +2,7 @@
 
 set -u
 
-sudo apt install -y gcc git python3 python3-dev zlib1g-dev make zip libssl-dev libbz2-dev liblzma-dev libcurl4-openssl-dev build-essential
+sudo apt install -y gcc git python3 python3-setuptools python3-dev zlib1g-dev make zip libssl-dev libbz2-dev liblzma-dev libcurl4-openssl-dev build-essential
 
 git clone https://github.com/raonyguimaraes/pynnotator
 cd pynnotator
