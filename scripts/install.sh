59fc1437e92745a3e973fab39a6c325e7b66f799
Raony Guimaraes <raonyguimaraes@gmail.com>
2019-08-14 10:45:05 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,8 @@
+sudo apt install python3-venv
+git clone https://github.com/raonyguimaraes/pynnotator
+cd pynnotator
+python3 -m venv venv
+source venv/bin/activate
 pip install wheel
 pip install -r requirements.txt
 python setup.py develop
